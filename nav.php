d785671a6784ec7e709df70fad41cbdefbbc279a
yito2 <yito2@u.rochester.edu>
2016-09-27 17:33:57 -0400

    Colophon file change

--- nav.php
+++ nav.php
@@ -3,5 +3,5 @@
 	<li <?php if($thisPage=="page1") { echo 'class="active"'; } ?>><a href="subtopic1.php">Subtopic1</a></li>
 	<li <?php if($thisPage=="page2") { echo 'class="active"'; } ?>><a href="subtopic2.php">Subtopic2</a></li>
 	<li <?php if($thisPage=="page3") { echo 'class="active"'; } ?>><a href="subtopic3.php">Subtopic3</a></li>
-	<li <?php if($thisPage=="colophon") { echo 'class="active"'; } ?>><a href="colophon.php">Colophon</a></li>
+	<li <?php if($thisPage=="colophon") { echo 'class="active"'; } ?>><a href="humans.txt">Colophon</a></li>
 </ul>
