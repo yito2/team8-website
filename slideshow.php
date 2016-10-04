93ba9989d6561724f103817323d503a761672868
rvandyke27 <rvandyke27@gmail.com>
2016-10-04 16:34:42 -0400

    More slideshow stuff

---
+++ slideshow.php
@@ -0,0 +1,22 @@
+<div class="slideshow-elements">
+	<figure>
+		<img src="images/WelcometoNYR.jpg" width="100%" />
+		<figcaption>Caption TBD</figcaption>
+	</figure>
+
+	<figure>
+		<img src="images/NYRlogo.jpg" width="100%" />
+		<figcaption>Caption TBD</figcaption>
+	</figure>
+
+	<figure>
+		<img src="images/NewYorkRangersTeamPic.jpg" width="100%" />
+		<figcaption>Caption TBD</figcaption>
+	</figure>
+
+	<figure>
+		<img src="images/EasternConference.jpg" width="100%" />
+		<figcaption>Caption TBD</figcaption>
+	</figure>
+
+</div>
---
+++ slideshowtest.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>Javascript slideshow test</title>
+	<link rel="stylesheet" href="css/slideshow.css">
+</head>
+
+<body>
+<div class="container">
+
+		<?php include "slideshow.php" ; ?>
+
+
+</div><!--.container-->
+</body>
+</html>
