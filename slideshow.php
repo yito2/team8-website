86a63b7d947c8dec57a1d6e12a5c4176d1612b7a
rvandyke27 <rvandyke27@gmail.com>
2016-10-08 09:29:09 -0400

    Added slideshow CSS
    Still have to plus in slideshow js

--- slideshow.php
+++ slideshow.php
@@ -1,22 +1,45 @@
-<div class="slideshow-elements">
-	<figure>
-		<img src="images/WelcometoNYR.jpg" width="100%" />
-		<figcaption>Caption TBD</figcaption>
-	</figure>
+<div class="slideshow-container">
+	<div class ="mySlides fade">
+		<div class="numbertext">1 / 4</div>
+		<figure>
+			<img src="images/WelcometoNYR.jpg" width="100%" />
+			<figcaption>Caption TBD</figcaption>
+		</figure>
+	</div>
 
-	<figure>
-		<img src="images/NYRlogo.jpg" width="100%" />
-		<figcaption>Caption TBD</figcaption>
-	</figure>
+	<div class="mySlides fade">
+		<div class="numbertext">2 / 4</div>
+		<figure>
+			<img src="images/NYRlogo.jpg" width="100%" />
+			<figcaption>Caption TBD</figcaption>
+		</figure>
+	</div>
 
-	<figure>
-		<img src="images/NewYorkRangersTeamPic.jpg" width="100%" />
-		<figcaption>Caption TBD</figcaption>
-	</figure>
+	<div class="mySlides fade">
+		<div class="numbertext">3 / 4</div>
+		<figure>
+			<img src="images/NewYorkRangersTeamPic.jpg" width="100%" />
+			<figcaption>Caption TBD</figcaption>
+		</figure>
+	</div>
 
-	<figure>
-		<img src="images/EasternConference.jpg" width="100%" />
-		<figcaption>Caption TBD</figcaption>
-	</figure>
+	<div class="mySlides fade">
+		<div class="numbertext">4 /4</div>
+		<figure>
+			<img src="images/EasternConference.jpg" width="100%" />
+			<figcaption>Caption TBD</figcaption>
+		</figure>
+	</div>
 
+	<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+	<a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+</div>
+<br>
+
+<div style="text-align:center">
+	<span class="dot" onclick="currentSlide(1)"></span>
+	<span class="dot" onclick="currentSlide(2)"></span>
+	<span class="dot" onclick="currentSlide(3)"></span>
+	<span class="dot" onclick="currentSlide(4)"></span>
 </div>
