bc7b7fd9b7aa1cfc33f7dd9a1e10633e36c65874
rvandyke27 <rvandyke27@gmail.com>
2016-10-11 17:50:47 -0400

    Added space for tasks completed
    Everyone put in the specific tasks you completed! Just expanding on
    content from humans.txt

--- Colophon.php
+++ Colophon.php
@@ -20,31 +20,37 @@
 				<tr class="trow">
 					<th class="theader">Name</th>
 					<th class="theader">Role</th>
+					<th class="theader">Tasks Completed</th>
 					<th class="theader">Class Year</th>
 				</tr>
 				<tr class="trow">
 					<td class="tdescription">Yukako Ito</td>
 					<td class="tdescription">Architect</td>
+					<td class="tdescription"><p></p></td>
 					<td class="tdescription">2017</td>
 				</tr>
 				<tr class="trow">
 					<td class="tdescription">Annie Zhang</td>
 					<td class="tdescription">Framework Wrangler</td>
+					<td class="tdescription"><p></p></td>
 					<td class="tdescription">2017</td>
 				</tr>
 				<tr class="trow">
 					<td class="tdescription">Carolyn Zelicof</td>
 					<td class="tdescription">Content Master and Designer</td>
+					<td class="tdescription"><p></p></td>
 					<td class="tdescription">2019</td>
 				</tr>
 				<tr class="trow">
 					<td class="tdescription">Rebecca Van Dyke</td>
 					<td class="tdescription">JavaScript Coder</td>
+					<td class="tdescription"><p>Fully implemented slideshow using JavaScript and CSS. Slideshow scrolls automatically and user can jump between pictures using the dots at the bottom</p></td>
 					<td class="tdescription">2017</td>
 				</tr>
 				<tr class="trow">
 					<td class="tdescription">Jie Deng</td>
 					<td class="tdescription">CSS Artist</td>
+					<td class="tdescription"><p></p></td>
 					<td class="tdescription">2017</td>
 				</tr>
 			</table>		
