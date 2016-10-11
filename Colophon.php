6939739de8099cb4f2d7efaf9f8a976e8e92d4c9
Jie Deng <dengjie@dhcp-10-5-22-159.wireless.rochester.edu>
2016-10-11 17:44:58 -0400

    Colophon Page
    Colophon page as required in the pdf

---
+++ Colophon.php
@@ -0,0 +1,58 @@
+<!-- colophon page that introduces the	team members-->
+
+<!doctype html>
+<?php $thisPage="page4"; ?>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Colophon</title>
+	
+	<?php include("inc/top.inc");?>
+
+	<!--Probably use a php include for the hero/header-->
+
+	<?php include("inc/nav.inc");?>
+	<article>
+		<h2>Team Members</h2>
+		<table class="table-bordered">
+				<caption class="tcaption"><strong><em>Team 8 Members</em></strong></caption>
+				<tr class="trow">
+					<th class="theader">Name</th>
+					<th class="theader">Role</th>
+					<th class="theader">Class Year</th>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">Yukako Ito</td>
+					<td class="tdescription">Architect</td>
+					<td class="tdescription">2017</td>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">Annie Zhang</td>
+					<td class="tdescription">Framework Wrangler</td>
+					<td class="tdescription">2017</td>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">Carolyn Zelicof</td>
+					<td class="tdescription">Content Master and Designer</td>
+					<td class="tdescription">2019</td>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">Rebecca Van Dyke</td>
+					<td class="tdescription">JavaScript Coder</td>
+					<td class="tdescription">2017</td>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">Jie Deng</td>
+					<td class="tdescription">CSS Artist</td>
+					<td class="tdescription">2017</td>
+				</tr>
+			</table>		
+	</article>
+	</div> <!-- container -->
+
+</body>
+
+<?php include("inc/footer.inc");?>
+
+</html>
